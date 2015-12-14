557cd02653187f3f558250e68c1b31e51ea20fac
DavidFiumano <Mynameisspam1@gmail.com>
2015-12-14 15:20:21 -0500

    Adding broken encryption.

--- Debug/src/MistOperate.d
+++
@@ -1,8 +0,0 @@
-src/MistOperate.o: ../src/MistOperate.cpp ../src/../Headers/Mist.h \
- ../src/../Headers/Task.h ../src/../Headers/Mist.h
-
-../src/../Headers/Mist.h:
-
-../src/../Headers/Task.h:
-
-../src/../Headers/Mist.h:
--- Debug/src/Task.d
+++
@@ -1,6 +0,0 @@
-src/Task.o: ../src/Task.cpp ../src/../Headers/Task.h \
- ../src/../Headers/Mist.h
-
-../src/../Headers/Task.h:
-
-../src/../Headers/Mist.h:
